8ac8287be29006f9273835472f4de608043362c4
Gonzalo <gonzalososa342@gmail.com>
2024-10-27 17:58:12 -0300

    Clase batallon para agregar personajes

---
+++ src/clases/Batallon.java
@@ -0,0 +1,21 @@
+package clases;
+
+import clases.personajes.Personaje;
+
+public class Batallon {
+
+	public void agregarPersonaje(Personaje crearMago) {
+		
+	}
+
+	public boolean tienePersonajesSaludables() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void atacar(Batallon batallonMortifagos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

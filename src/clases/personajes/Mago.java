e294c5ac67b89d9a06480377d5acc0ddfc35101a
Gonzalo <gonzalososa342@gmail.com>
2024-10-27 17:54:43 -0300

    Clases mago y mortifago que heredan de personaje

---
+++ src/clases/personajes/Mago.java
@@ -0,0 +1,10 @@
+package clases.personajes;
+
+// hechizos defensivos y de ataque
+public class Mago extends Personaje {
+
+	public Mago(String nombre, double puntosDeVida, int nivelDeMagia) {
+		super(nombre, puntosDeVida, nivelDeMagia);
+	}
+
+}
---
+++ src/clases/personajes/Mortifago.java
@@ -0,0 +1,10 @@
+package clases.personajes;
+
+// ataques oscuros y habilidades especiales
+public class Mortifago extends Personaje {
+
+	public Mortifago(String nombre, double puntosDeVida, int nivelDeMagia) {
+		super(nombre, puntosDeVida, nivelDeMagia);
+	}
+
+}

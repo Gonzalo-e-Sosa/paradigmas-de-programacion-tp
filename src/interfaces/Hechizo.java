b7e6dc3413390e7cca13429a13d13e496af6611f
gvuono <82596693+gvuono@users.noreply.github.com>
2024-11-11 15:21:51 -0300

    Update Hechizo.java

--- src/interfaces/Hechizo.java
+++ src/interfaces/Hechizo.java
@@ -3,6 +3,10 @@ package interfaces;
 import clases.personajes.Personaje;
 
 public interface Hechizo {
-	void ejecutar(Personaje lanzador, Personaje objetivo);
-	boolean esOfensivo();
+    void ejecutar(Personaje lanzador, Personaje objetivo);
+    boolean esOfensivo();
+    
+    // Agregamos la constante que cada hechizo implementará
+    int NIVEL_REQUERIDO = 0;  // Valor por defecto, que puede ser sobrescrito en cada hechizo
 }
+
